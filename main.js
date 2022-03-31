b112e6535998f3d333ab88bd0adaccf6eb64b588
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-03-31 13:05:47 +0600

    ok

---
+++ main.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('button');
+var main = document.getElementById('main');
+var text = document.getElementById('text');
+function on(){
+  button.classList.toggle('on');
+  main.classList.toggle('dark');
+  text.classList.toggle('dark');
+}
